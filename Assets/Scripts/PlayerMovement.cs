f6d88ad261d9284f8327d77cb5a6f42777a2847c
noah <noah@gitlab.cs470.local>
2022-10-01 17:16:09 -0700

    Added Basic Player Controls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public GameObject player;
+	public Rigidbody rb;
+
+	public float speed = .1f;
+	public float jumpSpeed = 1f;
+
+	Vector3 moveDirection;
+	Vector3 lookRotate;	
+
+	bool isGround;
+
+	void Start() 
+	{
+		isGround = false;
+	}	
 
     // Update is called once per frame
     void Update()
     {
-        
+		moveDirection.x = Input.GetAxisRaw("Horizontal");
+		if (Input.GetButtonDown("Jump"))
+		{
+			Debug.Log("Jump Registered");
+			Jump();
+		}
     }
+
+	void FixedUpdate()
+	{
+		rb.MovePosition(rb.position + moveDirection * speed * Time.fixedDeltaTime);
+	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.gameObject.tag == "Platform")
+		{
+			Debug.Log("isGrounded");
+			isGround = true;
+		}
+		else
+		{
+			isGround = false;
+		}
+	}
+
+	void Jump()
+	{
+		if (isGround)
+		{
+			float jumpVelocity = jumpSpeed * Time.fixedDeltaTime;
+			rb.velocity = new Vector3( 0, jumpVelocity, 0);
+			isGround = false;
+			Debug.Log("jump");
+		}
+	}
 }
